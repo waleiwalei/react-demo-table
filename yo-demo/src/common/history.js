862498b5ad8bd3e27d2d991b2ac3867abe391121
waleiwalei <1284848557@qq.com>
2018-08-22 11:34:13 +0800

    PromiseRealize

--- yo-demo/src/common/history.js
+++
@@ -1,8 +0,0 @@
-import { createHashHistory } from '$router';
-
-const yoHistory = createHashHistory({
-    uniqueKey: 'create-unique-hybridId'
-    // spa: true // 默认是多页的，如果需要强制开启spa，可以设置这个属性为 true
-});
-
-export default yoHistory;
